ee7b15ed0b81b51ce436f71c975b0b0515e4391c
Sergei Golovnev <golovnevs@gmail.com>
2020-06-10 03:35:48 +0200

    Static Page with demo functionality

---
+++ js/script.js
@@ -0,0 +1,232 @@
+let outfits = [{
+  outfitId: 1,
+  imgSrc: 'images/1.jpg',
+  rank: 0,
+  rejectedCounter: 0,
+  items: ['whiteSneakers', 'blackSkinnyJeans'],
+  tags: ['sport', 'classic']
+},
+{
+  outfitId: 2,
+  imgSrc: 'images/2.jpg',
+  rank: 0,
+  rejectedCounter: 0,
+  items: ['whiteSneakers', 'blackSkinnyJeans'],
+  tags: ['sport', 'classic']
+},
+{
+  outfitId: 3,
+  imgSrc: 'images/3.jpg',
+  rank: 0,
+  rejectedCounter: 0,
+  items: ['whiteSneakers', 'blackSkinnyJeans'],
+  tags: ['sport', 'classic']
+},
+{
+  outfitId: 4,
+  imgSrc: 'images/4.jpg',
+  rank: 0,
+  rejectedCounter: 0,
+  items: ['whiteSneakers', 'blackSkinnyJeans'],
+  tags: ['sport', 'classic']
+},
+{
+  outfitId: 5,
+  imgSrc: 'images/5.jpg',
+  rank: 0,
+  rejectedCounter: 0,
+  items: ['whiteSneakers', 'blackSkinnyJeans'],
+  tags: ['sport', 'classic']
+},
+{
+  outfitId: 6,
+  imgSrc: 'images/6.jpg',
+  rank: 0,
+  rejectedCounter: 0,
+  items: ['whiteSneakers', 'blackSkinnyJeans'],
+  tags: ['sport', 'classic']
+},
+{
+  outfitId: 7,
+  imgSrc: 'images/7.jpg',
+  rank: 0,
+  rejectedCounter: 0,
+  items: ['whiteSneakers', 'blackSkinnyJeans'],
+  tags: ['sport', 'classic']
+},
+{
+  outfitId: 8,
+  imgSrc: 'images/8.jpg',
+  rank: 0,
+  rejectedCounter: 0,
+  items: ['whiteSneakers', 'blackSkinnyJeans'],
+  tags: ['sport', 'classic']
+},
+{
+  outfitId: 9,
+  imgSrc: 'images/9.jpg',
+  rank: 0,
+  rejectedCounter: 0,
+  items: ['whiteSneakers', 'blackSkinnyJeans'],
+  tags: ['sport', 'classic']
+},
+{
+  outfitId: 10,
+  imgSrc: 'images/10.jpg',
+  rank: 0,
+  rejectedCounter: 0,
+  items: ['whiteSneakers', 'blackSkinnyJeans'],
+  tags: ['sport', 'classic']
+},
+{
+  outfitId: 11,
+  imgSrc: 'images/11.jpg',
+  rank: 0,
+  rejectedCounter: 0,
+  items: ['whiteSneakers', 'blackSkinnyJeans'],
+  tags: ['sport', 'classic']
+},
+{
+  outfitId: 12,
+  imgSrc: 'images/12.jpg',
+  rank: 0,
+  rejectedCounter: 0,
+  items: ['whiteSneakers', 'blackSkinnyJeans'],
+  tags: ['sport', 'classic']
+}
+];
+
+let l = 0;
+let r = 1;
+
+function displayQuiz() {
+
+quizDiv = document.createElement("div");
+quizDiv.setAttribute("class", "quiz");
+const main = document.getElementById('main');
+if (l <= 5 && r <= 5) {
+  main.innerHTML = `
+  <h1 class="mainHeader">What would you prefer?</h1>
+  <div class="quiz">
+    <div id="leftPic" class="picWrapper">
+      <a>
+        <img src=${outfits[l].imgSrc} class="image">
+      </a>
+      <div class="middle">
+        <div class="text">❤️</div>
+      </div>
+    </div>
+    <div id="rightPic" class="picWrapper">
+      <a>
+        <img src=${outfits[r].imgSrc} class="image">
+      </a>
+      <div class="middle">
+        <div class="text">❤️</div>
+      </div>
+    </div>
+  </div>
+  `;
+} else {
+  main.innerHTML = `
+  <h2>Here are the most liked outfits</h2>
+  <h3>[Tags for outfits]</h3>
+  <div class="quiz">
+    <a id="leftRes" class="picWrapper">
+      <img src=${outfits[0].imgSrc} class="image">
+    </a>
+    <a id="centerRes" class="picWrapper">
+      <img src=${outfits[5].imgSrc} class="image">
+    </a>
+    <a id="rightRes" class="picWrapper">
+      <img src=${outfits[6].imgSrc} class="image">
+    </a>
+  </div>        
+  `;
+  
+  resultLeft = document.getElementById("leftRes"); 
+  resultLeft.addEventListener('click', function() {
+    main.innerHTML = `
+    <h2>Here are the most liked outfits</h2>
+    <h3>[Tags for outfits]</h3>
+    <div class="quiz">
+      <a id="leftRes" class="picWrapper">
+        <img src=${outfits[0].imgSrc} class="image">
+      </a>
+      <a id="centerRes" class="picWrapper">
+        <img src=${outfits[5].imgSrc} class="image">
+      </a>
+      <a id="rightRes" class="picWrapper">
+        <img src=${outfits[6].imgSrc} class="image">
+      </a>
+    </div>
+    <h1>[links for outfit 1]</h1>
+    `;
+  });
+
+  resultCenter = document.getElementById("centerRes");
+  resultCenter.addEventListener('click', function() {
+    main.innerHTML = `
+    <h2>Here are the most liked outfits</h2>
+    <h3>[Tags]</h3>
+    <div class="quiz">
+      <a id="leftRes" class="picWrapper">
+        <img src=${outfits[0].imgSrc} class="image">
+      </a>
+      <a id="centerRes" class="picWrapper">
+        <img src=${outfits[5].imgSrc} class="image">
+      </a>
+      <a id="rightRes" class="picWrapper">
+        <img src=${outfits[6].imgSrc} class="image">
+      </a>
+    </div>
+    <h1>[links for outfit 2]</h1>
+    `;
+  });
+
+  resultRight = document.getElementById("rightRes");
+  resultRight.addEventListener('click', function() {
+    main.innerHTML = `
+    <h2>Here are the most liked outfits</h2>
+    <h3>[Tags]</h3>
+    <div class="quiz">
+      <a id="leftRes" class="picWrapper">
+        <img src=${outfits[0].imgSrc} class="image">
+      </a>
+      <a id="centerRes" class="picWrapper">
+        <img src=${outfits[5].imgSrc} class="image">
+      </a>
+      <a id="rightRes" class="picWrapper">
+        <img src=${outfits[6].imgSrc} class="image">
+      </a>
+    </div>
+    <h1>[links for outfit 3]</h1>
+    `;
+  });
+}
+
+main.appendChild(quizDiv);
+voteRight = document.getElementById("rightPic");
+voteRight.addEventListener('click', voteCounterRight);
+voteLeft = document.getElementById("leftPic");
+voteLeft.addEventListener('click', voteCounterLeft);
+};
+
+const startBtn = document.getElementById('start');
+startBtn.addEventListener('click', displayQuiz);
+
+function voteCounterLeft() {
+if (r == l - 1) {
+  r = r + 2;
+  displayQuiz();
+} else
+  r++;
+displayQuiz();
+};
+
+function voteCounterRight() {
+if (l == r - 1) {
+  l = l + 2;
+  displayQuiz();
+} else l++;
+displayQuiz();
+};
